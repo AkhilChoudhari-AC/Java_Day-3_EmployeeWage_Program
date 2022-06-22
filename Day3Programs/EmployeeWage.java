78031f5fd968f989286f8fb7dee8f59d4ee90767
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-06-22 12:33:57 +0530

    Calculating Daily Employee Wages

--- Day3Programs/EmployeeWage.java
+++ Day3Programs/EmployeeWage.java
@@ -6,14 +6,17 @@ public class EmployeeWage {
     public static void main(String[] args) {
 
         int present = 1;
+        int WagePerHr = 20;
+        int FullDayHr = 8;
         Random rand = new Random();
         int empCheck = rand.nextInt(2);
         if (empCheck == present) {
             System.out.println("Employee is present");
+            System.out.println("Employee Daily Wages for full Day is :" +(WagePerHr * FullDayHr));
         }
-    else
+    else {
             System.out.println("Employee is Absent");
-
-
+            System.out.println("Employee Daily Wages is : 0");
+        }
     }
 }
