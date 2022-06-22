480d53ad727ed007bb0480c638eaef1c78871bc7
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-06-22 14:29:55 +0530

    Calculating Wages For a Month

--- Day3Programs/EmployeeWage.java
+++ Day3Programs/EmployeeWage.java
@@ -7,22 +7,28 @@ public class EmployeeWage {
 
        // int FullPresent = 1;
       //  int PartTime = 2;
+        int WorkDayPerMonth = 20;
         int WagePerHr = 20;
-        int FullDayHr = 8;
-        int PartTimeHr = 4;
+      //  int FullDayHr = 0;
+       // int PartTimeHr = 0;
+       int EmpHr = 0;
+        for (int day = 0; day < WorkDayPerMonth; day++){
         Random rand = new Random();
         int empCheck = rand.nextInt(3);
      //   if (empCheck == FullPresent) {
+
         switch (empCheck) {
             case 1:
+                EmpHr = 8;
                 System.out.println("Employee is full time present");
-                System.out.println("Employee Daily Wages for full Day is :" + (WagePerHr * FullDayHr));
+                System.out.println("Employee Daily Wages for full Day is :" + (WagePerHr * EmpHr));
                 break;
 
             //   } else if (empCheck == PartTime) {
             case 2:
+                EmpHr = 4;
                 System.out.println("Employee is part time present");
-                System.out.println("Employee Daily wages for part time is :" + (WagePerHr * PartTimeHr));
+                System.out.println("Employee Daily wages for part time is :" + (WagePerHr * EmpHr));
                 break;
             //   } else {
             default: {
@@ -30,5 +36,8 @@ public class EmployeeWage {
                 System.out.println("Employee Daily Wages is : 0");
             }
         }
+        }
+        int TotalWageOfMonth = (WagePerHr * EmpHr * WorkDayPerMonth);
+        System.out.println("Total wages of a Employee for a Month IS :" +TotalWageOfMonth);
     }
 }
