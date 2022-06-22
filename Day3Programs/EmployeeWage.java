d958bfb4dd456756f48a892a9543b2b8c7f0be5a
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-06-22 13:35:35 +0530

    Solving using Switch Case Statement

--- Day3Programs/EmployeeWage.java
+++ Day3Programs/EmployeeWage.java
@@ -1,28 +1,34 @@
 package Day3Programs;
 
-import java.sql.SQLOutput;
 import java.util.Random;
 
 public class EmployeeWage {
     public static void main(String[] args) {
 
-        int FullPresent = 1;
-        int PartTime = 2;
+       // int FullPresent = 1;
+      //  int PartTime = 2;
         int WagePerHr = 20;
         int FullDayHr = 8;
         int PartTimeHr = 4;
         Random rand = new Random();
         int empCheck = rand.nextInt(3);
-        if (empCheck == FullPresent) {
-            System.out.println("Employee is full time present");
-            System.out.println("Employee Daily Wages for full Day is :" +(WagePerHr * FullDayHr));
-        } else if (empCheck == PartTime) {
-            System.out.println("Employee is part time present");
-            System.out.println("Employee Daily wages for part time is :" +(WagePerHr * PartTimeHr));
+     //   if (empCheck == FullPresent) {
+        switch (empCheck) {
+            case 1:
+                System.out.println("Employee is full time present");
+                System.out.println("Employee Daily Wages for full Day is :" + (WagePerHr * FullDayHr));
+                break;
 
-        } else {
-            System.out.println("Employee is Absent");
-            System.out.println("Employee Daily Wages is : 0");
+            //   } else if (empCheck == PartTime) {
+            case 2:
+                System.out.println("Employee is part time present");
+                System.out.println("Employee Daily wages for part time is :" + (WagePerHr * PartTimeHr));
+                break;
+            //   } else {
+            default: {
+                System.out.println("Employee is Absent");
+                System.out.println("Employee Daily Wages is : 0");
+            }
         }
     }
 }
