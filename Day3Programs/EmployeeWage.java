4019ac47a6e82c2e65ddf3e691b7531a0fef8b19
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-06-22 13:14:18 +0530

    Adding part time employee and wages

--- Day3Programs/EmployeeWage.java
+++ Day3Programs/EmployeeWage.java
@@ -1,20 +1,26 @@
 package Day3Programs;
 
+import java.sql.SQLOutput;
 import java.util.Random;
 
 public class EmployeeWage {
     public static void main(String[] args) {
 
-        int present = 1;
+        int FullPresent = 1;
+        int PartTime = 2;
         int WagePerHr = 20;
         int FullDayHr = 8;
+        int PartTimeHr = 4;
         Random rand = new Random();
-        int empCheck = rand.nextInt(2);
-        if (empCheck == present) {
-            System.out.println("Employee is present");
+        int empCheck = rand.nextInt(3);
+        if (empCheck == FullPresent) {
+            System.out.println("Employee is full time present");
             System.out.println("Employee Daily Wages for full Day is :" +(WagePerHr * FullDayHr));
-        }
-    else {
+        } else if (empCheck == PartTime) {
+            System.out.println("Employee is part time present");
+            System.out.println("Employee Daily wages for part time is :" +(WagePerHr * PartTimeHr));
+
+        } else {
             System.out.println("Employee is Absent");
             System.out.println("Employee Daily Wages is : 0");
         }
