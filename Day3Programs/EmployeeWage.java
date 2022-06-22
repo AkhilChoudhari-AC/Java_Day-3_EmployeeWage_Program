cce86cad9f46192272ba4a640990051f318a8a93
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-06-22 12:09:48 +0530

    Welcome to the Employee Wage Computation Program

---
+++ Day3Programs/EmployeeWage.java
@@ -0,0 +1,8 @@
+package Day3Programs;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Employee Wage Computation Program");
+    }
+}
